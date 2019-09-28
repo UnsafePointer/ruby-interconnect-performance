8b48a9654c2414ef428b8ed8787e54e2bf9f594f
Renzo Crisostomo <renzo@crisostomo.me>
2019-09-28 02:24:24 +0200

    Create output.txt file

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,21 @@
 #include <cstdint>
 #include <string>
 #include <memory>
+#include <sstream>
 
 int main(int argc, char* argv[]) {
     std::ifstream input = std::ifstream("input.txt");
+    std::stringstream ss = std::stringstream();
     std::unique_ptr<Interconnect> interconnect = std::make_unique<Interconnect>();
     for(std::string line; getline(input, line);) {
         uint32_t address = stoi(line);
         std::string result = interconnect->load(address);
+        ss << result;
+        ss << "\n";
     }
+    std::ofstream myfile = std::ofstream();
+    myfile.open("output.txt");
+    myfile << ss.str();
+    myfile.close();
     return 0;
 }
