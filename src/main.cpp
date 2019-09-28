77cb3c897fec77f5e0d8480c7d27e8fbef519239
Renzo Crisostomo <renzo@crisostomo.me>
2019-09-28 18:58:18 +0200

    Final commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <sstream>
 
 int main(int argc, char* argv[]) {
-    std::ifstream input = std::ifstream("input.txt");
+    std::ifstream input = std::ifstream("input-big.txt");
     std::stringstream ss = std::stringstream();
     std::unique_ptr<Interconnect> interconnect = std::make_unique<Interconnect>();
     for(std::string line; getline(input, line);) {
--- validate_results.py
+++ validate_results.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-with open('output_before.txt') as f1:
-    with open('output_after.txt') as f2:
+with open('output_before_big2.txt') as f1:
+    with open('output_after_big.txt') as f2:
         for cnt, line1 in enumerate(f1):
             line2 = f2.readline()
             if line1 != line2:
