452ac31e49898eadcff9344644cda97db9a260e3
Renzo Crisostomo <renzo@crisostomo.me>
2019-09-28 02:12:40 +0200

    Use Interconnect class

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include "Interconnect.hpp"
+#include <fstream>
+#include <cstdint>
+#include <string>
+#include <memory>
 
 int main(int argc, char* argv[]) {
+    std::ifstream input = std::ifstream("input.txt");
+    std::unique_ptr<Interconnect> interconnect = std::make_unique<Interconnect>();
+    for(std::string line; getline(input, line);) {
+        uint32_t address = stoi(line);
+        std::string result = interconnect->load(address);
+    }
     return 0;
 }
